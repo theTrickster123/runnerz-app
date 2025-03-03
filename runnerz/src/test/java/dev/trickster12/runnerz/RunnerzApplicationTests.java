9929d5a21a3f4f9945f5110aa0966f0b940009f1
theTrickster123 <tarissi.mehdi2019@gmail.com>
2025-03-03 21:35:35 +0000

    Resolved maven test error, and switch to Spring Data JDBC for managing my database (dependency added to my pom.xml)

--- runnerz/src/test/java/dev/trickster12/runnerz/RunnerzApplicationTests.java
+++ runnerz/src/test/java/dev/trickster12/runnerz/RunnerzApplicationTests.java
@@ -1,7 +1,9 @@
 package dev.trickster12.runnerz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class RunnerzApplicationTests {
